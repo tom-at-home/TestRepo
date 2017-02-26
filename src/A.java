61573211efc43b9f85681dd5d25bd21f3f442c4f
Tomasz <tomasz.urbaniak@me.com>
2017-02-26 13:42:39 +0100

    New AB

--- src/A.java
+++ src/A.java
@@ -1,12 +1,15 @@
 
 public class A {
 
-    public void TestB(){
+    public void TestA(){
+
+        System.out.println("TestA");
+
+    }
+    public void TestB() {
 
         System.out.println("TestB");
 
     }
-
-    System.out.println("Naechste Zeile");
 
 }
