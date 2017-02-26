4d1fb1057d1e88eb4e8369b76ac92e70e9b5219a
Tomasz <tomasz.urbaniak@me.com>
2017-02-26 13:29:19 +0100

    Wrong Version

--- src/A.java
+++ src/A.java
@@ -1,22 +1,11 @@
 
 public class A {
 
-    public void TestAAA(){
+    public void TestAAA() {
 
-        System.out.println("TestA");
-        System.out.println("TestCDE");
-
-        System.out.println("Noch ne Zeile");
-
-
-
-    public void TestB(){
-
-        System.out.println("TestB");
+        
 
     }
-
-    //System.out.println("Naechste Zeile");
 
 }
 
